bd55831e969eca85924bfb616abcfefde7dcad87
saibia8 <saibia.design@gmail.com>
2023-09-10 12:45:27 +0200

    Created listings from api call

--- src/js/api/ui/common/buildInfoIndex.mjs
+++ src/js/api/ui/common/buildInfoIndex.mjs
@@ -5,9 +5,20 @@ import { displayMessage } from "./displayMessage.mjs";
 import { displayAuctions } from "../templates/displayAuctions.mjs";
 
 export default async function buildInfoIndex() {
-  const menuDivLogin = document.getElementById("loginOrLogout");
   const container = document.getElementById("auctions");
+  const endpoint = "listings?_bids=true";
+
+  const { data, error } = await makeApiCall(endpoint);
+
+  if (error) {
+    return displayMessage("danger", error, container);
+  }
+
+  displayAuctions(data, container);
+
   if (isLoggedIn()) {
+    const menuDivLogin = document.getElementById("loginOrLogout");
+    const profile = getProfile();
     menuDivLogin.innerHTML = "";
     menuDivLogin.classList.remove("d-grid", "gap-3", "d-lg-block");
     menuDivLogin.classList.add(
@@ -16,33 +27,52 @@ export default async function buildInfoIndex() {
       "align-items-center",
       "gap-4"
     );
-    const profile = getProfile();
 
-    menuDivLogin.innerHTML = `
-    <div class="d-flex justify-content-center align-items-center gap-3">
-      <p class="text-white mb-0 text-uppercase fw-semibold">
-        Credits: ${profile.credits}
-      </p>
-      <a href="/user/profile/" class="nav-link"
-        ><span class="fs-3"><i class="bi bi-person-circle "></i></span></a>
-    </div>
-    <button class="log-out-btn" id="logout">
-      Log out<span>
-      <i class="bi bi-box-arrow-right"></i>
-      </span>
-    </button>
-    `;
+    const div = document.createElement("div");
+    div.classList.add(
+      "d-flex",
+      "justify-content-center",
+      "align-items-center",
+      "gap-3"
+    );
+    menuDivLogin.appendChild(div);
+
+    const p = document.createElement("p");
+    p.classList.add("text-white", "mb-0", "text-uppercase", "fw-semibold");
+    const textNodeCredits = document.createTextNode(
+      "Credits: " + profile.credits
+    );
+    p.appendChild(textNodeCredits);
+    div.appendChild(p);
+
+    const a = document.createElement("a");
+    a.href = "/user/profile/";
+    a.className = "nav-link";
+    div.appendChild(a);
+
+    const span = document.createElement("span");
+    span.className = "fs-3";
+    a.appendChild(span);
+
+    const i = document.createElement("i");
+    i.classList.add("bi", "bi-person-circle");
+    span.appendChild(i);
+
+    const btn = document.createElement("button");
+    btn.className = "log-out-btn";
+    btn.id = "logout";
+    btn.innerText = "Log out";
+    menuDivLogin.appendChild(btn);
+
+    const spanBtn = document.createElement("span");
+    btn.appendChild(spanBtn);
+
+    const iBtn = document.createElement("i");
+    iBtn.classList.add("bi", "bi-box-arrow-right");
+    spanBtn.appendChild(iBtn);
 
     listeners.setLogoutListener();
+
   } else {
-    const endpoint = "listings";
-
-    const { data, error } = await makeApiCall(endpoint);
-
-    if (error) {
-      return displayMessage("danger", error, container);
-    }
-
-    displayAuctions(data, container);
   }
 }
--- src/js/api/ui/templates/displayAuctions.mjs
+++ src/js/api/ui/templates/displayAuctions.mjs
@@ -1,13 +1,24 @@
+import { isLoggedIn } from "../../../helpers/storage.mjs";
+
 export function displayAuctions(auctions, container) {
   container.innerHTML = "";
   const html = auctions.map((auction) => {
-   console.log(auction);
-   return createAuction(auction);
-});
+    console.log(auction);
+    return createAuction(auction);
+  });
   container.append(...html);
 }
 
-function createAuction({ title, media }) {
+function createAuction({ title, media, endsAt, _count, bids }) {
+  const endDate = new Date(endsAt);
+  let totalAmount = 0;
+
+  bids.forEach((element) => {
+    if (element.amount > totalAmount) {
+      totalAmount = element.amount;
+    }
+  });
+
   const div = document.createElement("div");
   div.classList.add("col-10", "col-md-6", "col-lg-4", "col-xl-3", "mb-4");
 
@@ -32,6 +43,118 @@ function createAuction({ title, media }) {
   img.classList.add("card-img-top", "border-bottom");
   img.alt = `${title}`;
   divCard.appendChild(img);
-  
+
+  const divTime = document.createElement("div");
+  divTime.classList.add(
+    "d-flex",
+    "bg-white",
+    "justify-content-center",
+    "py-2",
+    "px-1",
+    "align-items-center",
+    "text-center"
+  );
+  divCard.appendChild(divTime);
+
+  const divTime2 = document.createElement("div");
+  divTime2.classList.add(
+    "d-flex",
+    "flex-fill",
+    "flex-wrap",
+    "flex-lg-nowrap",
+    "justify-content-center",
+    "align-items-center"
+  );
+  divTime.appendChild(divTime2);
+
+  const divDays = document.createElement("div");
+  divDays.classList.add("d-flex", "flex-column", "border-end", "px-2");
+  divTime2.appendChild(divDays);
+
+  const pDaysNum = document.createElement("p");
+  pDaysNum.className = "mb-0";
+  pDaysNum.textContent = endDate.getDay();
+  divDays.appendChild(pDaysNum);
+  const pDays = document.createElement("p");
+  pDays.className = "mb-0";
+  pDays.innerText = "days";
+  divDays.appendChild(pDays);
+
+  const divHours = document.createElement("div");
+  divHours.classList.add("d-flex", "flex-column", "border-end", "px-2");
+  divTime2.appendChild(divHours);
+
+  const pHoursNum = document.createElement("p");
+  pHoursNum.className = "mb-0";
+  pHoursNum.textContent = endDate.getHours();
+  divHours.appendChild(pHoursNum);
+  const pHours = document.createElement("p");
+  pHours.className = "mb-0";
+  pHours.innerText = "hours";
+  divHours.appendChild(pHours);
+
+  const divMinutes = document.createElement("div");
+  divMinutes.classList.add("d-flex", "flex-column", "border-end", "px-2");
+  divTime2.appendChild(divMinutes);
+
+  const pMinutesNum = document.createElement("p");
+  pMinutesNum.className = "mb-0";
+  pMinutesNum.textContent = endDate.getMinutes();
+  divMinutes.appendChild(pMinutesNum);
+  const pMinutes = document.createElement("p");
+  pMinutes.className = "mb-0";
+  pMinutes.innerText = "minutes";
+  divMinutes.appendChild(pMinutes);
+
+  const divSeconds = document.createElement("div");
+  divSeconds.classList.add("d-flex", "flex-column", "border-end", "px-2");
+  divTime2.appendChild(divSeconds);
+
+  const pSecondsNum = document.createElement("p");
+  pSecondsNum.className = "mb-0";
+  pSecondsNum.textContent = endDate.getSeconds();
+  divSeconds.appendChild(pSecondsNum);
+  const pSeconds = document.createElement("p");
+  pSeconds.className = "mb-0";
+  pSeconds.innerText = "seconds";
+  divSeconds.appendChild(pSeconds);
+
+  const divInfo = document.createElement("div");
+  divInfo.classList.add("card-body", "py-4", "bg-light");
+  divCard.appendChild(divInfo);
+
+  const h3Title = document.createElement("h3");
+  h3Title.classList.add("card-title", "fs-5", "fw-semibold", "mb-3");
+  h3Title.innerText = title;
+  divInfo.appendChild(h3Title);
+
+  const pBid = document.createElement("p");
+  pBid.classList.add("fs-6", "mb-1");
+  pBid.innerText = "Current bid:";
+  divInfo.appendChild(pBid);
+
+  const pCreditsBids = document.createElement("p");
+  pCreditsBids.classList.add("fs-6", "mb-4", "fw-medium");
+  pCreditsBids.innerHTML = `${totalAmount} <span class="text-uppercase">credits</span> (${_count.bids} bids)`;
+  divInfo.appendChild(pCreditsBids);
+
+  const aBid = document.createElement("a");
+  if (isLoggedIn()) {
+    aBid.href = "#";
+    aBid.innerText = "Place a bid";
+  } else {
+    aBid.href = "/user/register/";
+    aBid.innerText = "Register to bid";
+  }
+  aBid.classList.add(
+    "btn",
+    "btn-primary",
+    "rounded-pill",
+    "btn-style",
+    "btn-green",
+    "btn-bid"
+  );
+  divInfo.appendChild(aBid);
+
   return div;
 }
